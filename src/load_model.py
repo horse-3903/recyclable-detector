5e78e07f8efd4e27d1db3712f88774e8d4a96448
horse-3903 <chongchoonhourafael@gmail.com>
2024-10-15 13:55:58 +0800

    v0.2.0

---
+++ src/load_model.py
@@ -0,0 +1,35 @@
+import cv2
+from ultralytics import YOLO
+
+# Load the trained YOLOv8 model
+model = YOLO("./src/model.pt")  # Replace with your trained model path
+
+# Open the webcam (0 is the default webcam)
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open webcam")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Error: Could not read frame from webcam")
+        break
+
+    # Run the YOLO model on the frame
+    results = model.predict(source=frame)
+
+    # Draw the bounding boxes and labels on the frame
+    annotated_frame = results[0].plot()
+
+    # Display the frame with detections
+    cv2.imshow('YOLOv8 Live Detection', annotated_frame)
+
+    # Break the loop when 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close the window
+cap.release()
+cv2.destroyAllWindows()
---
+++ src/train.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.append("../data")
+
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+model.train(data="./data/data.yaml", epochs=200, imgsz=640, batch=16, amp=False)
+model_path = "model.pt"
+model.save(model_path)
+print(f"Model saved to {model_path}")
